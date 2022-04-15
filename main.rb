039dc35bce195209e1e278896b68d53c3423b74c
mattand <mjande@umich.edu>
2022-04-15 09:38:51 -0400

    Finished generate_guess and clean up errors

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@
 # Feedback Colors: White (correct color but wrong spot),
 # Black (correct color and correct spot)
 
+require 'pry-byebug'
+
 BLUE = ' blue   '
 YELLOW = ' yellow '
 GREEN = ' green  '
@@ -59,14 +61,15 @@ class Game
   def play_human_round(round)
     puts "Guess ##{round}!"
     guess = player.input_guess
-    feedback = computer.check_guess(guess)
+    feedback = computer.check_guess(guess, computer.secret_code)
     board.update_board(round, guess, feedback)
     end_human_game(feedback, round)
   end
 
   def play_computer_round(round)
-    guess = computer.input_guess(round)
-    feedback = computer.check_guess(guess)
+    guess = computer.generate_guess
+    feedback = computer.check_guess(guess, computer.secret_code)
+    computer.previous_feedback = feedback
     board.update_board(round, guess, feedback)
     end_computer_game(feedback, round)
   end
@@ -158,6 +161,15 @@ class Player
     puts 'Type four letters and press enter to submit your secret code.'
     secret_code = gets.chomp.downcase.split('')
     convert_color(secret_code)
+    if secret_code.length != 4
+      puts 'Error: please enter four of the letters shown above.'
+      input_secret_code
+    elsif secret_code.any?('ERROR')
+      puts 'Error: please enter four of the letters shown above.'
+      input_secret_code
+    else
+      secret_code
+    end
   end
 
   def convert_color(array)
@@ -185,51 +197,140 @@ end
 # data: secret code
 # methods: generate code, check code
 class Computer
-  attr_accessor :secret_code, :feedback, :possible_guesses
+  attr_accessor :secret_code, :previous_feedback, :possible_guesses, :previous_guess
+
+  def initialize
+    possible_guesses = (1111..6666).to_a
+    possible_guesses.map! do |guess|
+      guess.to_s.chars.map(&:to_i)
+    end
+    possible_guesses.reject! { |guess| guess.include?(0) }
+    @possible_guesses = possible_guesses
+    @previous_feedack = ['']
+    @previous_guess = nil
+  end
 
   def generate_secret_code
     secret_code = []
-    colors = [BLUE, YELLOW, GREEN, PURPLE, ORANGE, RED]
     4.times do |num|
-      secret_code[num] = colors.sample
+      secret_code[num] = rand(1..6)
     end
     @secret_code = secret_code
   end
 
   def store_player_code(secret_code)
-    @secret_code = secret_code
-    @possible_guesses = (1111..6666).to_a
+    secret_code_i = secret_code.map do |str|
+      case str
+      when BLUE
+        1
+      when YELLOW
+        2
+      when GREEN
+        3
+      when PURPLE
+        4
+      when ORANGE
+        5
+      when RED
+        6
+      end
+    end
+    @secret_code = secret_code_i
   end
 
-  def input_guess
-    case feedback
-    when nil
-      guess = 1122
-    when [0,0]
-      
+  def generate_guess
+    p previous_feedback
+    if previous_feedback.nil?
+      guess = [1, 1, 2, 2]
+    else
+      possible_guesses.delete(previous_guess)
+      possible_guesses.select! do |possible_guess|
+        check_guess(previous_guess, possible_guess) == previous_feedback
+      end
+      guess = minmax(possible_guesses)
+    end
+    @possible_guesses = possible_guesses
+    @previous_guess = guess
+    guess_to_s(previous_guess)
   end
 
+  def minmax(possible_guesses)
+    possible_score_totals = Hash.new(0)
+    possible_guesses.each do |possible_guess|
+      possible_score = check_guess(previous_guess, possible_guess)
+      possible_score_totals[possible_score] += 1
+    end
+    p possible_score_totals
+    best_score = possible_score_totals.min { |a, b| a[1] <=> b[1] }
+    p best_score
+    next_guess = (possible_guesses.find { |guess| check_guess(previous_guess, guess) == best_score[0] })
+    p next_guess
+    next_guess
+  end
 
-  def check_guess(guess)
+  def guess_to_i(str_array)
+    guess_i = str_array.map do |str|
+      case str
+      when BLUE
+        1
+      when YELLOW
+        2
+      when GREEN
+        3
+      when PURPLE
+        4
+      when ORANGE
+        5
+      when RED
+        6
+      end
+    end
+    @guess = guess_i
+  end
+
+  def guess_to_s(int_array)
+    guess_s = int_array.map do |int|
+      case int
+      when 1
+        BLUE
+      when 2
+        YELLOW
+      when 3
+        GREEN
+      when 4
+        PURPLE
+      when 5
+        ORANGE
+      when 6
+        RED
+      end
+    end
+    @guess = guess_s
+  end
+
+  def check_guess(guess, code)
+    guess = guess_to_i(guess) if guess[0].instance_of? String
     num_of_black = 0
     num_of_white = 0
-    colors = [BLUE, YELLOW, GREEN, PURPLE, ORANGE, RED]
+    colors = (1..6).to_a
     colors.each do |color|
-      if secret_code.include?(color)
-        secret_code_color = secret_code.count(color)
+      if code.include?(color)
+        code_count = code.count(color)
+        guess_color_count = guess.count(color)
         guess.each_index do |i|
-          if guess[i] == color && secret_code[i] == color
+          if guess[i] == color && code[i] == color
             num_of_black += 1
-            secret_code_color -= 1
+            code_count -= 1
+            guess_color_count -= 1
           end
         end
-        while secret_code_color.positive?
-          num_of_white += 1 if guess.count(color) >= secret_code_color
-          secret_code_color -= 1
+        while code_count.positive?
+          num_of_white += 1 if guess.count(color) >= code_count
+          code_count -= 1
         end
       end
     end
-    @internal_feedback = [num_of_black, num_of_white]
+    [num_of_black, num_of_white]
   end
 end
 
@@ -237,3 +338,6 @@ def play_again?
 end
 
 Game.new
+
+# To-do: fix guess conversion (Do I need to store as instance variable?)
+# finish minmax algorithm
